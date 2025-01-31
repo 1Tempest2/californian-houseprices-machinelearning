5bd7363f29910cc4cebddc26778aea378cce9117
Bábinszki Botond <babinszkibotond@gmail.com>
2025-01-31 16:57:15 +0100

    Cleaned Data

--- linear-regression.py
+++ linear-regression.py
@@ -6,6 +6,14 @@ import scipy.stats as stats
 import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-file_path = "C:/Users/Botond/PycharmProjects/californian-houseprices-machinelearning/Data/housing.csv/housing.csv"
-data = pd.read_csv(file_path)
-print(data.columns)
+path = "C:/Users/Botond/PycharmProjects/californian-houseprices-machinelearning/Data/housing.csv/housing.csv"
+data = pd.read_csv(path)
+#print(data["ocean_proximity"].unique())
+missing_values = data.isnull().sum()
+#print(missing_values)
+missing_percentage = missing_values * 100 / len(data)
+#print(missing_percentage)
+cleaned_data = data.dropna()
+#print(cleaned_data.isnull().sum())
+#print(data.describe())
+# visualizing house values
